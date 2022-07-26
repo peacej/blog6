c3f17a959055146a735fdd8049dd9b39b1818c84
Jerry Chi <jchi@indeed.com>
2022-07-26 16:42:26 +0900

    update content

--- buildpush.sh
+++ buildpush.sh
@@ -1,10 +1,8 @@
+pelican content -s publishconf.py
 git add . 
 git commit -m "update content"
-pelican content -s publishconf.py
-git checkout main
 git push origin main
 ghp-import output -b gh-pages
 git checkout gh-pages
-git pull
 git push origin gh-pages
 git checkout main
